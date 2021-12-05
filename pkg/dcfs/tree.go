cb6beff20d8dad0dd0196218008ea212e452ec9e
Alejandro Mery <amery@jpi.io>
2021-12-05 22:34:16 +0000

    dcfs: make Directory{} implement Node{}
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- pkg/dcfs/tree.go
+++ pkg/dcfs/tree.go
@@ -8,11 +8,19 @@ import (
 	"go.sancus.dev/fs"
 )
 
+var (
+	_ Node = (*Directory)(nil)
+)
+
 type Directory struct {
 	entry   *NodeEntry
 	content *NodeDirectoryContent
 	tree    *radix.Tree
 }
+
+// Node
+func (dir *Directory) Inode() uint64  { return dir.entry.Inode }
+func (dir *Directory) Type() NodeType { return dir.entry.Content.Type() }
 
 func (fsys *Filesystem) Open(name string) (fs.File, error) {
 	return nil, syscall.ENOENT
